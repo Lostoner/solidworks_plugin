482e2151a09407a6414b936e50692106cb0007bd
Lostoner <1151710764@qq.com>
2020-09-18 20:53:45 +0800

    Build the project
    Project built
    & function(ConnectToSolidWorks)
    completed.

---
+++ solidworks_plugin/solidworks_plugin/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace solidworks_plugin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doc_class.ConnectToSolidWorks();
+        }
+    }
+}
---
+++ solidworks_plugin/solidworks_plugin/doc_class.cs
@@ -0,0 +1,59 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace solidworks_plugin
+{
+    public class doc_class
+    {
+        public static ISldWorks SwApp { get; private set; }
+
+        public static ISldWorks ConnectToSolidWorks()
+        {
+            if(SwApp != null)
+            {
+                return SwApp;
+            }
+            Debug.Print("Connect to solidworks...");
+            try
+            {
+                SwApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            }
+            catch (COMException)
+            {
+                try
+                {
+                    SwApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application.23");//2015
+                }
+                catch (COMException)
+                {
+                    try
+                    {
+                        SwApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application.26");//2018
+                    }
+                    catch (COMException)
+                    {
+                        MessageBox.Show("Could not connect to SolidWorks.", "SolidWorks", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        SwApp = null;
+                    }
+                }
+            }
+            if(SwApp != null)
+            {
+                Debug.Print("Connection succeed.");
+                return SwApp;
+            }
+            else
+            {
+                Debug.Print("Connection failed.");
+                return null;
+            }
+        }
+    }
+}
